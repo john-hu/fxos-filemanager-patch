d193ecdbcbddee45aa378fbba8ddbb7635142e12
John Hu <im@john.hu>
2015-07-01 07:04:50 +0800

    update icons and rewrite the logic

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-window.addEventListener('contextmenu', function(e) {
-  e.preventDefault();
-});
+(function() {
+  function preventContextMenu(e) {
+    e.preventDefault();
+  }
+
+  function check() {
+    if (document.querySelectorAll('*[contextmenu]').length === 0) {
+      if (!window.oncontextmenu) {
+        window.oncontextmenu = preventContextMenu;
+      }
+    } else {
+      if (window.oncontextmenu === preventContextMenu) {
+        window.oncontextmenu = null;
+      }
+    }
+  }
+  var mo = new MutationObserver(function() {
+    check();
+  });
+  mo.observe(document.documentElement, {
+    childList: true,
+  });
+
+  check();
+})();
